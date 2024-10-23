6d6102fac8c145fd8cf3404938a08c2b884a18d4
tipej <tiitus.jarvinen@gmail.com>
2024-10-23 14:05:08 +0300

    Added first version of UDPclient

---
+++ UDPclient.java
@@ -0,0 +1,54 @@
+import java.io.Console;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+
+public class UDPclient {
+    public static void main(String[] args) {
+        String serverURL = "";
+        int serverPort = -1;
+        if (args.length != 2) {
+            System.err.println("Usage: java UDPclient <URL> <PORT>");
+            return;
+        }
+
+        try {
+            serverURL = args[0];
+            serverPort = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            System.err.println("Invalid URL or PORT");
+            return;
+        }
+
+        System.out.println("Enter text lines to send to the server. Enter an empty line to stop.");
+
+        String line = null;
+        while ((line = System.console().readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            sendLine(serverURL, serverPort, line);
+        }
+    }
+
+    public static void sendLine(String serverURL, int serverPort, String line) {
+        System.out.println("Sending line to server: " + line);
+        DatagramSocket socket = null;
+        try {
+            // Encode the line in 'utf-8'
+            // line = new String(line.getBytes(), "utf-8");
+            socket = new DatagramSocket();
+            byte[] data = line.getBytes();
+            InetAddress address = InetAddress.getByName(serverURL);
+            DatagramPacket packet = new DatagramPacket(data, data.length, address, serverPort);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+    
+}
